0715c4b0490ecf177bb7bd8633d44669f3e2b57d
xx <xxiunan@163.com>
2025-03-21 18:53:53 +0800

    Site updated: 2025-03-21 18:53:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/hello-world/","2025/03/20/408/数据结构/"];function toRandomPost(){
+var posts=["2025/03/20/hello-world/","2025/03/20/408/数据结构/","2025/03/20/高数/高数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
