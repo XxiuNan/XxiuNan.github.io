304f9e4119859c4c0ba2786d17c31a29cccfff36
xx <xxiunan@163.com>
2025-03-24 10:54:10 +0800

    Site updated: 2025-03-24 10:54:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/高数/","2025/03/20/数据结构/"];function toRandomPost(){
+var posts=["2025/03/20/数据结构/数据结构/","2025/03/20/高数/高数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
