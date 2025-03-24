f3f18eee16a1b888e87fe19bb844d40bb8824461
xx <xxiunan@163.com>
2025-03-24 10:48:29 +0800

    Site updated: 2025-03-24 10:48:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/hello-world/","2025/03/20/高数/","2025/03/20/数据结构/"];function toRandomPost(){
+var posts=["2025/03/20/高数/","2025/03/20/数据结构/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
